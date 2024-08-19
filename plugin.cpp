724325eceb04d625d18c54142ffdccaa9279177f
pintomax <massimiliano.pinto@gmail.com>
2024-08-19 08:42:36 +0200

    FOGL-9019: addition of mandatory and minimum for parameters
    FOGL-9019: addition of mandatory and minimum for parameters

--- plugin.cpp
+++ plugin.cpp
@@ -46,35 +46,43 @@ static const char *default_config = QUOTE({
 		"type" : "string",
 		"default" : DEFAULT_ASSETNAME_PREFIX,
 		"displayName" : "Asset Name prefix",
-		"order" : "1"
+		"order" : "1",
+		"mandatory": "true"
 		},
 	"master_id" : {
 		"description" : "Master Link Id",
 		"type" : "integer",
 		"default" : DEFAULT_MASTER_LINK_ID,
 		"displayName" : "Master link Id",
-		"order" : "2"
+		"order" : "2",
+		"maximum" : "65519",
+		"minimum" : "1"
 		},
 	"outstation_tcp_address" : {
 		"description" : "Outstation TCP/IP address",
 		"type" : "string",
 		"default" : DEFAULT_TCP_ADDR,
 		"displayName" : "Outstation address",
-		"order" : "3"
+		"order" : "3",
+		"mandatory": "true"
 		},
 	"outstation_tcp_port" : {
 		"description" : "Outstation TCP/IP port",
 		"type" : "integer",
 		"default" : DEFAULT_TCP_PORT,
 		"displayName" : "Outstation port",
-		"order" : "4"
+		"order" : "4",
+		"maximum" : "65000",
+		"minimum" : "1"
 		},
 	"outstation_id" : {
 		"description" : "Outstation Link Id",
 		"type" : "integer",
 		"default" : DEFAULT_OUTSTATION_ID,
 		"displayName" : "Outstation link Id",
-		"order" : "5"
+		"order" : "5",
+		"maximum" : "65519",
+		"minimum" : "1"
 		},
 	"outstation_scan_enable" : {
 		"description" : "Enable outstation data scan (Integrity Poll for all Classes)",
@@ -88,14 +96,16 @@ static const char *default_config = QUOTE({
 		"type" : "integer",
 		"default" : DEFAULT_OUTSTATION_SCAN_INTERVAL,
 		"displayName" : "Scan interval",
-		"order" : "7"
+		"order" : "7",
+		"minimum" : "1"
 		},
 	"data_fetch_timeout" : {
 		"description" : "Timeout in seconds while fetching data",
 		"type" : "integer",
 		"default" : DEFAULT_APPLICATION_TIMEOUT,
 		"displayName" : "Network timeout",
-		"order" : "8"
+		"order" : "8",
+		"minimum" : "1"
 		}
 	});
 
